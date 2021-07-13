bc63446aeb957b6e09ecb1f9b19031995d411477
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-07-13 11:31:27 +0700

    .

--- application/views/consent/tr_manage_training_configuration/v_manage_configuration.php
+++ application/views/consent/tr_manage_training_configuration/v_manage_configuration.php
@@ -66,7 +66,7 @@
 
                               <!-- Start Menu Manage Training Record -->
                               <div class="col-xl-4 col-md-6 mb-4">
-                                    <a href="">
+                                    <a href="<?php echo base_url() ?>tr_manage_training_configuration/Manage_training_course/Course">
                                           <div class="card border4 shadow h-100 py-2">
                                                 <div class="card-body">
                                                       <div class="row no-gutters align-items-center">
@@ -93,7 +93,7 @@
 
                               <!-- Start Menu Employee Training Record -->
                               <div class="col-xl-4 col-md-6 mb-4">
-                                    <a href="">
+                                    <a href="<?php echo base_url() ?>tr_manage_training_configuration/Manage_instructor/Instructor">
                                           <div class="card border4 shadow h-100 py-2">
                                                 <div class="card-body">
                                                       <div class="row no-gutters align-items-center">
