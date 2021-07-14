d9fdbe5e037750a9988a855c61381dab36b46941
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-07-14 10:20:28 +0700

    Update v_manage_training_course.php

--- application/views/consent/tr_manage_training_configuration/v_manage_training_course.php
+++ application/views/consent/tr_manage_training_configuration/v_manage_training_course.php
@@ -75,7 +75,7 @@
 
                                                             </div>
                                                             <div class="pull-right margin">
-                                                                  <button type="button" class="btn btn-success">Create
+                                                                  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#CreateModal">Create
                                                                         Course
                                                                         <i class="fa fa-plus text-black"></i></button>
                                                             </div>
@@ -170,3 +170,29 @@
                   </a>
             </div>
     
+           <!-- Modal -->
+            <div class="modal fade" id="CreateModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="mediumModalLabel">Medium Modal</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>
+                                There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra
+                                and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus
+                                Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more
+                                horse-like. All three belong to the genus Equus, along with other living equids.
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-primary">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!-- Create Modal -->
