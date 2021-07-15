d721349ad675bfbff1947531166459bf59b44580
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-07-15 14:15:43 +0700

    Update v_create_training_record.php

--- application/views/consent/tr_manage_training_record/v_create_training_record.php
+++ application/views/consent/tr_manage_training_record/v_create_training_record.php
@@ -20,8 +20,8 @@
       }
 
       #panel_th_home {
-            background-color:#00B050;
-           
+            background-color: #00B050;
+
       }
 
       #icon_main_menu {
@@ -95,7 +95,7 @@
 
 
                                                                   &nbsp;&nbsp;&nbsp;&nbsp; Training Name :
-                                                                  <div class="col-sm-4">
+                                                                  <div class="col-sm-3">
                                                                         <input type="text" class="form-control"
                                                                               id="grouptext"
                                                                               placeholder="Training Name">
@@ -262,19 +262,211 @@
 
                                                       </div>
                                                       <!-- card-body -->
+
+
                                                 </div>
                                           </div>
                                     </div>
-                                   
+
 
 
                               </div>
                         </div>
+                        <!-- content 1 -->
+
+                        <div class="content">
+                              <div class="animated fadeIn">
+                                    <div class="row">
+
+                                          <div class="col-md-12">
+                                                <div class="card">
+
+                                                      <div class="row">
+                                                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                  <input class="form-control col-md-2" type="text"
+                                                                        placeholder="Search ..." aria-label="Search">
+
+                                                            
+                                                                        &nbsp;&nbsp;
+                                                                 
+                                                                        <button type="button"
+                                                                              class="btn btn-success">Add Member
+                                                                              <i
+                                                                                    class="fa fa-plus text-black"></i></button>
+                                                                 
+                                                         
+                                                      </div>
+                                                      <div class="card-body">
+                                                            <table id="bootstrap-data-table"
+                                                                  class="table table-striped table-bordered">
+                                                                  <thead>
+                                                                        <tr align="center">
+
+                                                                              <th>No.</th>
+                                                                              <th>Course Code</th>
+                                                                              <th>Training Name</th>
+                                                                              <th>Training Description</th>
+                                                                              <th>Start-Date</th>
+                                                                              <th>End-Date</th>
+                                                                              <th>Trainer</th>
+                                                                              <th>Action</th>
+
+                                                                        </tr>
+                                                                  </thead>
+                                                                  <tbody>
+                                                                        <tr align="center">
+                                                                              <td>1.</td>
+                                                                              <td>IN-00001</td>
+                                                                              <td>General Safety</td>
+                                                                              <td>General Safety</td>
+                                                                              <td>02/01/2021</td>
+                                                                              <td>02/01/2021</td>
+                                                                              <td>Mr.Kenji Sleeptogether</td>
+                                                                              <td>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-info"><i
+                                                                                                class="fa fa-info-circle "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-warning"><i
+                                                                                                class="fa fa-edit "
+                                                                                                data-toggle="modal"
+                                                                                                data-target="#EditModal"></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-danger"><i
+                                                                                                class="ti ti-trash "
+                                                                                                data-toggle="modal"
+                                                                                                data-target="#DeleteModal"></i></button>
+                                                                              </td>
+                                                                        </tr>
+                                                                        <tr align="center">
+                                                                              <td>2.</td>
+                                                                              <td>IN-00002</td>
+                                                                              <td>Leader andd Management</td>
+                                                                              <td>Communication for Leader</td>
+                                                                              <td>21/01/2021</td>
+                                                                              <td>21/01/2021</td>
+                                                                              <td>Mr.Denial Ok</td>
+                                                                              <td>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-info"><i
+                                                                                                class="fa fa-info-circle "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-warning"><i
+                                                                                                class="fa fa-edit "
+                                                                                                data-toggle="modal"
+                                                                                                data-target="#EditModal"></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-danger"><i
+                                                                                                class="ti ti-trash "
+                                                                                                data-toggle="modal"
+                                                                                                data-target="#DeleteModal"></i></button>
+                                                                              </td>
+                                                                        </tr>
+                                                                        <tr align="center">
+                                                                              <td>3.</td>
+                                                                              <td>IN-00003</td>
+                                                                              <td>Professional Golf Caddie</td>
+                                                                              <td>Communication Efficiency</td>
+                                                                              <td>05/08/2021</td>
+                                                                              <td>05/08/2021</td>
+                                                                              <td>Mr.Kate J.son</td>
+                                                                              <td>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-info"><i
+                                                                                                class="fa fa-info-circle "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-warning"><i
+                                                                                                class="fa fa-edit "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-danger"><i
+                                                                                                class="ti ti-trash "></i></button>
+                                                                              </td>
+                                                                        </tr>
+                                                                        <tr align="center">
+                                                                              <td>4.</td>
+                                                                              <td>EX-00004</td>
+                                                                              <td>Building Good Attitude at Work</td>
+                                                                              <td>Self and Work Development</td>
+                                                                              <td>25/02/2022</td>
+                                                                              <td>25/02/2022</td>
+                                                                              <td>Mr.Tony Roger</td>
+                                                                              <td>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-info"><i
+                                                                                                class="fa fa-info-circle "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-warning"><i
+                                                                                                class="fa fa-edit"></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-danger"><i
+                                                                                                class="ti ti-trash "></i></button>
+                                                                              </td>
+                                                                        </tr>
+                                                                        <tr align="center">
+                                                                              <td>5.</td>
+                                                                              <td>EX-00005</td>
+                                                                              <td>Working as one team</td>
+                                                                              <td>Social skill for working together</td>
+                                                                              <td>07/03/2022</td>
+                                                                              <td>07/03/2022</td>
+                                                                              <td>Mr.Kenji Sleeptogether</td>
+                                                                              <td>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-info"><i
+                                                                                                class="fa fa-info-circle "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-warning"><i
+                                                                                                class="fa fa-edit "></i></button>
+                                                                                    <button type="button"
+                                                                                          class="btn btn-danger"><i
+                                                                                                class="ti ti-trash "></i></button>
+                                                                              </td>
+                                                                        </tr>
+
+                                                                  </tbody>
+                                                            </table>
+                                                      </div>
+                                                </div>
+                                          </div>
+
+
+                                    </div>
+                              </div><!-- .animated 2 -->
+                        </div><!-- .content 2 -->
+
                   </div>
+
+
+
+
+
+
+
+
+
             </div>
             <!-- Start col-lg-12 -->
 
-      </div>  <!-- /.container-fluid -->
+
+
+
+
+
+
+
+
+      </div> <!-- /.container-fluid -->
+
+
+
+
+
+
+
+
+
+
+
 
 
       <div class="col-md-3">
