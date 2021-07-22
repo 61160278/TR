6eef92eb9c518516d7b59491a27a13a192e87175
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-07-22 09:47:43 +0700

    .

--- application/views/consent/tr_report/v_report_training.php
+++ application/views/consent/tr_report/v_report_training.php
@@ -71,15 +71,18 @@
                                                       <div class="card-body">
 
                                                             <div class="row form-group">
-                                                                  <div class="col-sm-3">
+                                                                  <div class="col-sm-4">
 
                                                                         Employee Training Record Form :
+                                                                        <img src="<?php echo base_url();?>elaadmin/images/Excel.png" alt="Excel" height="8%">
                                                                         <br>
                                                                         <br>
                                                                         Employee Training Record By Department :
+                                                                        <img src="<?php echo base_url();?>elaadmin/images/Excel.png" alt="Excel" height="8%">
                                                                         <br>
                                                                         <br>
                                                                         Employee Training Record By Course :
+                                                                        <img src="<?php echo base_url();?>elaadmin/images/Excel.png" alt="Excel" height="8%">
 
                                                                   </div>
 
@@ -112,29 +115,4 @@
       <!-- /.container-fluid -->
 
 
-      <!-- Modal -->
-      <div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="staticModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-sm-5" role="document">
-                  <div class="modal-content">
-                        <div class="modal-header" style="background-color:#E74A3B;">
-                              <h5 class="modal-title" id="staticModalLabel"><b>
-                                          <font color="white" size="5px">Delete</font>
-                                    </b></h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                              </button>
-                        </div>
-                        <div class="modal-body">
-                              <p>
-                                    Please Confirm delete data.
-                              </p>
-                        </div>
-                        <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                              <button type="button" class="btn btn-primary">Confirm</button>
-                        </div>
-                  </div>
-            </div>
-      </div>
-      <!-- Delete Modal -->
+     
