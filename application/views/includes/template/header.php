b3f2721ff9c3d7ad486756f5cf3e4cad2281a718
61160278 <71628924+61160278@users.noreply.github.com>
2021-08-12 09:41:46 +0700

    Update header.php

--- application/views/includes/template/header.php
+++ application/views/includes/template/header.php
@@ -28,6 +28,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- End logo system  -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <!-- Start include stylesheet -->
     <link rel="apple-touch-icon" href="<?php echo base_url();?>elaadmin/images/Logo_TR.png">
     <link rel="shortcut icon" href="<?php echo base_url();?>elaadmin/images/Logo_TR.png">
