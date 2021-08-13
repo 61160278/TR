30ec93ae6c490314b9ae9406a42f6b38d37940e5
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-08-13 16:06:25 +0700

    Update v_create_training_record.php

--- application/views/consent/tr_manage_training_record/v_create_training_record.php
+++ application/views/consent/tr_manage_training_record/v_create_training_record.php
@@ -307,6 +307,55 @@
 
 
       }
+
+
+
+      function add_member_db() {
+
+var tr_course_code = document.getElementById("tr_course_code").value;
+var place_training = document.getElementById("place_training").value;
+var start_date = document.getElementById("start_date").value;
+var start_time = document.getElementById("start_time").value;
+var end_date = document.getElementById("end_date").value;
+var end_time = document.getElementById("end_time").value;
+var total_h = document.getElementById("total_h").value;
+var cost = document.getElementById("cost").value;
+var pre_score = document.getElementById("pre_score").value;
+var post_score = document.getElementById("post_score").value;
+
+
+
+// console.log(checkboxs)
+
+$.ajax({
+      type: "POST",
+      url: "<?php echo base_url(); ?>/tr_manage_training_record/Manage_training_record/add_training",
+      data: {
+            "tr_course_code": tr_course_code,
+            "place_training": place_training,
+            "start_date": start_date,
+            "start_time": start_time,
+            "end_date": end_date,
+            "end_time": end_time,
+            "total_h": total_h,
+            "cost": cost,
+            "pre_score": pre_score,
+            "post_score": post_score,
+         
+
+      },
+      dataType: "JSON",
+      success: function(data) {
+            // console.log(status)
+            // window.location.href =
+            //       "<?php echo base_url();?>/tr_manage_training_record/Manage_training_record/create_training_data";
+      }
+      // success function
+
+});
+
+// ajax 
+} //function add_courses
       </script>
 
       <!-- Begin Page Content -->
