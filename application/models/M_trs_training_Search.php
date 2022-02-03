f1aa89858bcde200d547a62fd625e4abb50088d9
Jirayukenmui2563 <61160053@go.buu.ac.th>
2022-02-03 14:23:17 +0700

    Update M_trs_training_Search.php

--- application/models/M_trs_training_Search.php
+++ application/models/M_trs_training_Search.php
@@ -126,7 +126,7 @@ class M_trs_training_Search extends trs_model {
 				ON ttd.trainer_id = ttr.Trainer_id
 				LEFT JOIN trs_database.trs_member_course as tmc
 				ON tmc.Training_ID = ttr.Training_id
-				
+
 				";
 				
 		$query = $this->db->query($sql);
