915e8780efaea5685f9191ef00543a451fc4b3b2
Jirayukenmui2563 <61160053@go.buu.ac.th>
2022-03-09 20:33:03 +0700

    Update v_report_group.php

--- application/views/consent/tr_report/v_report_group.php
+++ application/views/consent/tr_report/v_report_group.php
@@ -58,31 +58,130 @@
 
                         </div>
                         <!-- End header  -->
+                        <div class="col-md-12">
+                              <div class="card-body">
 
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
+                                    <br>
+                                    <div class="card-text text-sm-center">
+                                          <div class="row">
 
 
-            
-<div class="col-md-7">
-                  <a href="<?php echo base_url()?>tr_report/TR_report/Report">
-                        <button type="button" class="btn btn-secondary">Back</button>
-                  </a>
+                                               
+                                                <div class="col-md-3">
+                                                      <form action="<?php echo base_url() ?>tr_employee_training_record/Emp_training_record/Show_Profile"
+                                                            method="post">
+                                                            <input class="form-control" type="text" placeholder="Department"
+                                                                  name="emp_id" required>
+
+                                                </div>
+                                                <!--col3-->
+                                                <div class="col-md-3">
+                                                      <form action="<?php echo base_url() ?>tr_employee_training_record/Emp_training_record/Show_Profile"
+                                                            method="post">
+                                                            <input class="form-control" type="text" placeholder="Group"
+                                                                  name="emp_id" required>
+
+                                                </div>
+                                                <!--col3-->
+                                                <div class="col-md-3">
+                                                      <form action="<?php echo base_url() ?>tr_employee_training_record/Emp_training_record/Show_Profile"
+                                                            method="post">
+                                                            <input class="form-control" type="text" placeholder="Section"
+                                                                  name="emp_id" required>
+
+                                                </div>
+                                                <!--col3-->
+                                                <div class="col-md-1">
+                                                      <button class="btn btn-primary btn-lg" type="submit">
+                                                            <i class="fa  fa-search"></i>
+                                                </div>
+
+                                          </div>
+
+                                    </div>
+                                    <br>
+                                    <div class="row">
+
+                                          <table id="bootstrap-data-table" class="table table-striped table-bordered">
+                                                <thead>
+                                                      <tr align="center">
+                                                            <th>No.</th>
+                                                            <th>Course Code</th>
+                                                            <th>Training Name</th>
+                                                            <th>Training Description</th>
+                                                            <th>Start-Date</th>
+                                                            <th>End-Date</th>
+                                                            <th>Trainer</th>
+                                                            <th>Certificate</th>
+
+                                                      </tr>
+                                                </thead>
+                                                <tbody>
+
+                                                      <tr align="center">
+                                                            <td>1</td>
+                                                            <td>IN-0001</td>
+                                                            <td>General Safety</td>
+                                                            <td>How to Safe</td>
+                                                            <td>02/01/2021</td>
+                                                            <td>02/01/2021</td>
+                                                            <td>Mr.Jirayut</td>
+                                                            <td> &nbsp;&nbsp;
+                                                                  &nbsp;&nbsp;
+                                                                  &nbsp;<input type="checkbox" id="checkbox2"
+                                                                        name="checkbox2" value="option2"
+                                                                        class="form-check-input" checked></td>
+                                                      </tr>
+
+                                                      <tr align="center">
+                                                            <td>2</td>
+                                                            <td>IN-0002</td>
+                                                            <td>Data Analysis</td>
+                                                            <td>Data</td>
+                                                            <td>03/01/2021</td>
+                                                            <td>03/01/2021</td>
+                                                            <td>Mr.Terapong</td>
+                                                            <td> &nbsp;&nbsp;
+                                                                  &nbsp;&nbsp;
+                                                                  &nbsp;<input type="checkbox" id="checkbox2"
+                                                                        name="checkbox2" value="option2"
+                                                                        class="form-check-input" checked></td>
+                                                      </tr>
+
+
+
+                                                </tbody>
+                                          </table>
+                                    </div>
+
+                                    <br>
+                                    <div class="row">
+                                          <div class="col-md-8">
+                                                <a href="<?php echo base_url()?>tr_report/TR_report/Report">
+                                                      <button type="button" class="btn btn-secondary">Back</button>
+                                                </a>
+                                          </div>
+                                          <div class="col-md-2">
+                                                <img class="rounded-circle"
+                                                      src="<?php echo base_url();?>elaadmin/images/Excel.png"
+                                                      alt="Excel" width="55">
+                                                <button type="button" class="btn btn-primary">Dowload Excel</button>
+                                          </div>
+                                          <div class="col-md-2">
+                                                <img class="rounded-circle"
+                                                      src="<?php echo base_url();?>elaadmin/images/Excel.png"
+                                                      alt="Excel" width="55">
+                                                <button type="button" class="btn btn-primary">Dowload PDF</button>
+                                          </div>
+
+
+                                    </div>
+                                    <br>
+                              </div>
+                        </div>
+                        <!-- card-shadow mb-4 -->
+
+                  </div>
+                  <!-- col-lg-12 -->
             </div>
-
-
-            </div>
-            <!-- card-shadow mb-4 -->
-           
-      </div>
-      <!-- col-lg-12 -->
-      </div>
-      <!-- /.container-fluid -->
-
-
-     
+            <!-- /.container-fluid -->
