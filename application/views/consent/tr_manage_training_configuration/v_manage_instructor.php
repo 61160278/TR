2810daffb5a35d1e56ca3c41f9d4f3ac82704409
Jirayukenmui2563 <61160053@go.buu.ac.th>
2022-04-17 16:59:20 +0700

    Script

--- application/views/consent/tr_manage_training_configuration/v_manage_instructor.php
+++ application/views/consent/tr_manage_training_configuration/v_manage_instructor.php
@@ -654,3 +654,4 @@
       <?php 
 $num++;
 } //foreach?>
+  <script type="text/javascript" src="<?php echo base_url();?>DataTables/datatables.min.js"></script>
--- application/views/consent/tr_manage_training_configuration/v_manage_training_course.php
+++ application/views/consent/tr_manage_training_configuration/v_manage_training_course.php
@@ -827,3 +827,5 @@ console.log(tr_course_code)
       <?php 
 $num++;
 } //foreach?>
+
+<script type="text/javascript" src="<?php echo base_url();?>DataTables/datatables.min.js"></script>
